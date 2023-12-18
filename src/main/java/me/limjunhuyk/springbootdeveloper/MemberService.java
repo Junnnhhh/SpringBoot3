29515386594dca6f8cbf33d9c94dbcd0ddac7fd3
junnnhhh <imk1017@naver.com>
2023-12-18 22:52:41 +0900

    MemberService 추가

---
+++ src/main/java/me/limjunhuyk/springbootdeveloper/MemberService.java
@@ -0,0 +1,23 @@
+package me.limjunhuyk.springbootdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    @Autowired
+    MemberRepository memberRepository;
+
+    public void test() {
+        // 생성 (Create)
+        memberRepository.save(new Member(1L, "A"));
+
+        // 조회 (Read)
+        Optional<Member> member = memberRepository.findById(1L); // 단건 조회
+        List<Member> allMembers = memberRepository.findAll(); // 전체 조회
+
+        // 삭제 (Delete)
+        memberRepository.deleteById(1L);
+    }
+}
