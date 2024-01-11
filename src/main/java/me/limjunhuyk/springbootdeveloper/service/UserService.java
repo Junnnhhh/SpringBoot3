0b5f861022399699fa0d63c5969ca284f3fc5d9a
junnnhhh <imk1017@naver.com>
2024-01-11 23:14:40 +0900

    OAuth2로 로그인/로그아웃 구현하기 (2024-01-11)

--- src/main/java/me/limjunhuyk/springbootdeveloper/service/UserService.java
+++ src/main/java/me/limjunhuyk/springbootdeveloper/service/UserService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import me.limjunhuyk.springbootdeveloper.domain.User;
 import me.limjunhuyk.springbootdeveloper.dto.AddUserRequest;
 import me.limjunhuyk.springbootdeveloper.repository.UserRepository;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -24,5 +25,10 @@ public class UserService {
 
     public User findById(Long userId) {
         return userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
     }
 }
